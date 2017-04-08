0054b2530d11d9fa0fd7adf45b050329bfac0cf2
andrewgrano <andrewgrano@gmail.com>
2017-04-08 12:55:49 -0700

    re-compile older posts with updated code

--- public/js/templates.js
+++ public/js/templates.js
@@ -75,6 +75,12 @@ buf.push("<li>" + (jade.escape(null == (jade_interp = tag.name) ? "" : jade_inte
 }).call(this);
 
 buf.push("</ul></div></a></div>");}.call(this,"post" in locals_for_with?locals_for_with.post:typeof post!=="undefined"?post:undefined,"undefined" in locals_for_with?locals_for_with.undefined:typeof undefined!=="undefined"?undefined:undefined));;return buf.join("");
+},"category": function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+;var locals_for_with = (locals || {});(function (post) {
+buf.push("<div class=\"postWidget\"><span class=\"postWidget__title\">" + (jade.escape(null == (jade_interp = post.name) ? "" : jade_interp)) + "</span></div>");}.call(this,"post" in locals_for_with?locals_for_with.post:typeof post!=="undefined"?post:undefined));;return buf.join("");
 },"post": function template(locals) {
 var buf = [];
 var jade_mixins = {};
@@ -151,11 +157,5 @@ buf.push("<li>" + (jade.escape(null == (jade_interp = tag.name) ? "" : jade_inte
 }).call(this);
 
 buf.push("</ul></div></a></div>");}.call(this,"post" in locals_for_with?locals_for_with.post:typeof post!=="undefined"?post:undefined,"undefined" in locals_for_with?locals_for_with.undefined:typeof undefined!=="undefined"?undefined:undefined));;return buf.join("");
-},"category": function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-;var locals_for_with = (locals || {});(function (post) {
-buf.push("<div class=\"postWidget\"><span class=\"postWidget__title\">" + (jade.escape(null == (jade_interp = post.name) ? "" : jade_interp)) + "</span></div>");}.call(this,"post" in locals_for_with?locals_for_with.post:typeof post!=="undefined"?post:undefined));;return buf.join("");
 }};
 });
